3b9b1cb6ff271f284506fecbb5440222ba99d147
Ronnbreizh <ronnbreizh@gmail.com>
2024-03-18 09:45:48 +0100

    📃 Make logging configurable in env

---
+++ src/logging.rs
@@ -0,0 +1,19 @@
+use log::LevelFilter;
+
+/// Returns the log level defined in the env.
+/// If not defined, returns `log::LevelFilter::info`.
+pub fn log_level() -> LevelFilter {
+    let log_level = std::env::var("LOG_LEVEL")
+        .unwrap_or("INFO".into());
+
+    match log_level.to_uppercase().as_str() {
+        "OFF" =>LevelFilter::Off,
+        "INFO" => LevelFilter::Info,
+        "WARN" => LevelFilter::Warn,
+        "ERROR" => LevelFilter::Error,
+        "DEBUG"|"DBG" => LevelFilter::Debug,
+        "TRACE" => LevelFilter::Trace,
+        // By default, Info
+        _ => LevelFilter::Info,
+    }
+}
